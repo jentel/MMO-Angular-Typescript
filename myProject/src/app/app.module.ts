655e98036e6d85ee848ce90c66d8e841a427a11a
Jennifer Telisczak <JenniferT@magenic.com>
2018-07-18 10:39:30 -0500

    -added files based on introductory article

--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { AdminModule } from './admin/admin.module';
+import { ConvertToEuroPipe } from './convert-to-euro.pipe';
+import { BackendApiService } from './backend-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    ConvertToEuroPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdminModule
   ],
-  providers: [],
+  providers: [BackendApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
