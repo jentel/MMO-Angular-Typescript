eba467842a359700352a164ea55b082f180647a1
Jennifer Telisczak <JenniferT@magenic.com>
2018-07-19 16:37:35 -0500

    -completed "Services" Tutorial

--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
+
+import { MessageService } from './message.service';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ myProject/src/app/message.service.ts
@@ -0,0 +1,18 @@
+// service exposes its cache of messages
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
