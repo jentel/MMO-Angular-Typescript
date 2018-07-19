d7a7433ff13092652d1a7484726a8ac15274cc32
Jennifer Telisczak <JenniferT@magenic.com>
2018-07-18 22:04:42 -0500

    -added generic classes

--- myProject/src/app/generics.ts
+++ myProject/src/app/generics.ts
@@ -1,3 +1,8 @@
+class GenericNumber<T> {
+    zeroValue: T;
+    add:(x: T, y:T) => T;
+}
+
 interface GenericIdentityFn<T> {
     (arg: T): T;
 }
@@ -19,5 +24,15 @@ let output = identity<string>("myString"); // or identity("myString") will work 
 //let myIdentity: {<T>(arg: T): T} = identity;
 
 let myIdentity: GenericIdentityFn<number> = identity;
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function(x,y) {return x + y; };
+
+let stringNumeric = new GenericNumber<string>();
+stringNumeric.zeroValue = "";
+stringNumeric.add = function(x, y) { return x + y; };
+
+alert(stringNumeric.add(stringNumeric.zeroValue, "test"));
 
 document.body.innerHTML = identity(output);
