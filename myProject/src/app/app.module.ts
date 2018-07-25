36f8675b2d40d6616356cdbbe0362c8b49c3c4e2
Jennifer Telisczak <JenniferT@magenic.com>
2018-07-25 15:25:35 -0500

    -Completed "Routing" Tutorial

--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
-
 import { MessageService } from './message.service';
 import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     MessagesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [
     MessageService,
--- myProject/src/app/heroes/heroes.component.ts
+++ myProject/src/app/heroes/heroes.component.ts
@@ -11,13 +11,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-
-  selectedHero: Hero;
   
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -27,5 +21,4 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
      this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes); // updated for observable
   }
-
 }
